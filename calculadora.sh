0d8bbe1c492060a4bc6c9d9766109cf614722235
webviny360 <webviny@gmail.com>
2024-11-26 21:53:53 -0300

    Create calculadora.sh
    - Hospede seu código em Python no Github

---
+++ calculadora.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Bem-vindo à calculadora!"
+echo "Digite o primeiro número:"
+read num1
+echo "Digite o operador (+, -, *, /):"
+read operador
+echo "Digite o segundo número:"
+read num2
+
+case $operador in
+    +) resultado=$((num1 + num2));;
+    -) resultado=$((num1 - num2));;
+    \*) resultado=$((num1 * num2));;
+    /) resultado=$((num1 / num2));;
+    *) echo "Operador inválido"; exit 1;;
+esac
+
+echo "O resultado é: $resultado"
+
